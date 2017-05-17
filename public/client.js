10722b177b21dde6c649d6aa8bb14e62e6b0e9f2
Keith Underwood <keithunderwood510@gmail.com>
2017-05-17 14:11:53 -0700

    Rework client submitting to include league and team data

--- public/client.js
+++ public/client.js
@@ -1,14 +1,23 @@
 const inputSelector = document.getElementById('phone-input')
+const leagueSelector = document.getElementById('league')
+const teamSelector = document.getElementById('team')
 const submitSelector = document.getElementById('phone-submit')
 const formSelector = document.getElementById('phone-form')
 
 formSelector.addEventListener('submit', (e) => {
   e.preventDefault()
+  const league = leagueSelector.value
+  const team = teamSelector.value
   const phoneNumber = inputSelector.value
+  const data = {league, team, phoneNumber}
 
-  fetch(`/submit/${phoneNumber}`, {
+  fetch(`/submit`, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
     method: 'POST',
-    body: phoneNumber
+    body: JSON.stringify(data)
   })
     .then(response => console.log(response))
     .catch(err => console.error(err))
