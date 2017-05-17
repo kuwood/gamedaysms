2f0e3c883cc47d8699ad4f898f592b3af1d995b2
Keith Underwood <keithunderwood510@gmail.com>
2017-05-17 09:59:16 -0700

    Add express with submit phoneNumber endpoint

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const config = require('./config.js').config
-const accountSid = config.ACCOUNTSID
-const authToken = config.AUTHTOKEN
+require('dotenv').config()
+const accountSid = process.env.ACCOUNTSID
+const authToken = process.env.AUTHTOKEN
 
 const twilio = require('twilio')
 const client = new twilio(accountSid, authToken)
 const cronJob = require('cron').CronJob
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+app.post('/submit/:phoneNumber', (req, res) => {
+  console.log(req.params.phoneNumber)
+  if (req.params.phoneNumber === process.env.TESTPHONENUMBER) {
+    client.messages.create({
+      to: process.env.TESTPHONENUMBER,
+      from: process.env.YOURTWILIONUMBER,
+      body:'Test!'
+    })
+      .then(message => console.log(err, message.sid))  
+    res.sendStatus(201)
+  }
+  else res.sendStatus(400)
+})
+
+// build and connect firebase DB
+// setup xmlstats api
 
 const checkDay = new cronJob('0 12 * * *', () => {
   client.messages.create({
-    to: config.TESTPHONENUMBER,
-    from: config.YOURTWILIONUMBER,
+    to: process.env.TESTPHONENUMBER,
+    from: process.env.YOURTWILIONUMBER,
     body:'Test!'
   })
     .then(message => console.log(err, message.sid))  
 }, null, true)
 
+app.listen(3000, console.log('listening on 3000'))
