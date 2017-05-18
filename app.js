f0af0d3e6feac4e01bf26138861d366d6e6e3563
Keith Underwood <keithunderwood510@gmail.com>
2017-05-18 16:59:27 -0700

    Newline private key regex fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const admin = require('firebase-admin')
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: process.env.FIREBASE
